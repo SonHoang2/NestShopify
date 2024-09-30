067a7f725940ba5a04b1531a7e1b7a513fa0b45e
Hoàng Sơn <naruto3285@gmail.com>
2024-09-30 15:16:02 +0700

    update unit test

--- server/src/auth/auth.controller.spec.ts
+++ server/src/auth/auth.controller.spec.ts
@@ -1,18 +1,32 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AuthController } from './auth.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
 
-// describe('AuthController', () => {
-//     let controller: AuthController;
+describe('AuthController', () => {
+    let controller: AuthController;
+    let fakeAuthService: Partial<AuthService>;
+    let fakeUserService: Partial<UsersService>;
 
-//     beforeEach(async () => {
-//         const module: TestingModule = await Test.createTestingModule({
-//             controllers: [AuthController],
-//         }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AuthController],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: fakeAuthService
+                },
+                {
+                    provide: UsersService,
+                    useValue: fakeUserService
+                }
+            ],
+        }).compile();
 
-//         controller = module.get<AuthController>(AuthController);
-//     });
+        controller = module.get<AuthController>(AuthController);
+    });
 
-//     it('should be defined', () => {
-//         expect(controller).toBeDefined();
-//     });
-// });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+});
--- server/src/categories/categories.service.spec.ts
+++ server/src/categories/categories.service.spec.ts
@@ -1,18 +1,31 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CategoriesService } from './categories.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesService } from './categories.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from './category.entity';
+import { CategoryImage } from './category-image.entity';
 
-// describe('CategoriesService', () => {
-//   let service: CategoriesService;
+describe('CategoriesService', () => {
+    let service: CategoriesService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [CategoriesService],
-//     }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CategoriesService,
+                {
+                    provide: getRepositoryToken(Category),
+                    useValue: {}
+                },
+                {
+                    provide: getRepositoryToken(CategoryImage),
+                    useValue: {}
+                },
+            ],
+        }).compile();
 
-//     service = module.get<CategoriesService>(CategoriesService);
-//   });
+        service = module.get<CategoriesService>(CategoriesService);
+    });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+});
