000ad2e83b4cb64adb5220293992c56fdf1663b0
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2024-10-01 19:40:22 +0700

    update unit test

--- server/src/vouchers/vouchers.controller.spec.ts
+++ server/src/vouchers/vouchers.controller.spec.ts
@@ -1,18 +1,25 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { VouchersController } from './vouchers.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { VouchersController } from './vouchers.controller';
+import { VouchersService } from './vouchers.service';
 
-// describe('VouchersController', () => {
-//   let controller: VouchersController;
+describe('VouchersController', () => {
+    let controller: VouchersController;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [VouchersController],
-//     }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [VouchersController],
+            providers: [
+                {
+                    provide: VouchersService,
+                    useValue: {},
+                }
+            ]
+        }).compile();
 
-//     controller = module.get<VouchersController>(VouchersController);
-//   });
+        controller = module.get<VouchersController>(VouchersController);
+    });
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+});
